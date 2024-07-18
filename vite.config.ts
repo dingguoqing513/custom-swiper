2ef77f943a339fd240cf4aaa56d55a70b4f76baa
dingguoqing513 <yamuyy139@126.com>
2024-07-18 14:19:56 +0800

    chore: modify <dist> to <docs> for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
+  build: {
+    outDir: "docs", // for github page
+  },
   plugins: [vue()],
 });
